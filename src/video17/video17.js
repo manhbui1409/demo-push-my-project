afa4b2eb4bacac37e245ba3089775991cd0c13e2
Bui Tien Manh <buitienmanh1409@gmail.com>
2026-02-25 00:06:40 +0700

    cap nhatjs

--- src/video17/video17.js
+++ src/video17/video17.js
@@ -1,1 +1,16 @@
 console.log('Hello, Video 17!');
+var name = 'manh';
+const name1 = 'deptari';
+let age = 18;
+age = 30;
+const myAge = 25;
+
+console.log(age);
+{
+    var manh = 'deptrai';
+    let hieu = 'ngu dot';
+    console.log(manh);
+    console.log(hieu);
+}
+console.log(manh);
+console.log(hieu);
