822d3fc9a584c35e04c7c653e75a84b404712956
Bui Tien Manh <buitienmanh1409@gmail.com>
2026-02-28 22:01:12 +0700

    cap nhat js

--- src/video17/video17.js
+++ src/video17/video17.js
@@ -4,13 +4,24 @@ const name1 = 'deptari';
 let age = 18;
 age = 30;
 const myAge = 25;
+let tong = age + myAge;
 
-console.log(age);
-{
-    var manh = 'deptrai';
-    let hieu = 'ngu dot';
-    console.log(manh);
-    console.log(hieu);
-}
-console.log(manh);
-console.log(hieu);
+
+// console.log(tong);
+// {
+//     var manh = 'deptrai';
+//     let hieu = 'ngu dot'
+//     console.log(manh);
+//     console.log(hieu);
+// }
+// // console.log(manh);
+// // const isManhDepTrai = true;
+// // console.log(isManhDepTrai);
+// // let i;
+// // console.log(i); (khi chua dinh nghia bien ma khong gan gia tri thi no se la undefined);
+// // null la khi ta da dinh nghia roi va gan cho no gia tri la null;
+// let a = 5;
+// const b = a++;
+// console.log(a); // 6
+// console.log(b); // 5
+// //  nếu b = ++a thì b lúc này =6; còn a =5;
